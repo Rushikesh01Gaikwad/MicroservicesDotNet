9633e9869ce4ce9dc7e9c83360cec91b05d97232
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2025-05-28 08:38:18 +0530

    controller in progress

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static playFileService.Dtos.Dtos;
+
+namespace playFileService.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+      private static readonly List<ItemDto> items = new List<ItemDto>
+      {
+          new ItemDto(Guid.NewGuid(), "Item1", "Description1", 10.99m, DateTimeOffset.UtcNow),
+          new ItemDto(Guid.NewGuid(), "Item2", "Description2", 20.99m, DateTimeOffset.UtcNow),
+          new ItemDto(Guid.NewGuid(), "Item3", "Description3", 30.99m, DateTimeOffset.UtcNow)
+      };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ItemDto>> GetItems()
+        {
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
+            var item = items.FirstOrDefault(i => i.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto createItemDto)
+        {
+            var newItem = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(newItem);
+            return newItem;
+                /*CreatedAtAction(nameof(GetItem), new { id = newItem.Id }, newItem);*/
+        }
+    }
+}
---
+++ Dtos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace playFileService.Dtos
+{
+    public class Dtos
+    {
+        public record ItemDto (Guid Id, string Name, string Description, decimal Price, DateTimeOffset Date);
+        public record CreateItemDto(string Name, string Description, decimal Price);
+        public record UpdateItemDto(string Name, string Description, decimal Price);
+    }
+}
